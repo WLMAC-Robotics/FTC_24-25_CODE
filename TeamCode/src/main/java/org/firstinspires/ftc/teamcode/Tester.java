f15dead06276510cde4f5c9320694d3d0b325b58
Sehan Munir <162743866+Emera1d3x@users.noreply.github.com>
2024-12-05 11:32:08 -0500

    Updated Tester.java
    Better test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tester.java
@@ -4,38 +4,53 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "test")
+@TeleOp(name = "CyberLyons")
 public class Tester extends OpMode {
-    DcMotor motorArm;
-    DcMotor motorLWheel;
-    DcMotor motorRWheel;
+    DcMotor motorArm; // The Arm Motor
+    DcMotor motorLFWheel; // Left Front Wheel
+    DcMotor motorLBWheel; // Left Back Wheel
+    DcMotor motorRFWheel; // Right Front Wheel
+    DcMotor motorRBWheel; // Right Back Wheel
+    float leftStickX;
+    float leftStickY;
+    float rightStickX;
+    float rightStickY;
+
+
+    public void initializeStuff(){
+        motorArm = hardwareMap.get(DcMotor.class, "motorArm");
+        motorLFWheel = hardwareMap.get(DcMotor.class, "motorLF");
+        motorLBWheel = hardwareMap.get(DcMotor.class, "motorLB");
+        motorRFWheel = hardwareMap.get(DcMotor.class, "motorRF");
+        motorRBWheel = hardwareMap.get(DcMotor.class, "motorRB");
+    }
 
     @Override
     public void init() {
-        motorArm = hardwareMap.get(DcMotor.class, "motorArm");
-        motorLWheel = hardwareMap.get(DcMotor.class, "motorLeft");
-        motorRWheel = hardwareMap.get(DcMotor.class, "motorRight");
-        telemetry.addData("Initialization:", "does this work?");
+        telemetry.addData("Launch Test:", "Successful");
         telemetry.update();
-        System.out.println("HELP");
     }
 
     @Override
     public void loop() {
+        // DEFAULTS
         motorArm.setPower(0);
-        motorLWheel.setPower(0);
-        motorRWheel.setPower(0);
-        if (gamepad1.right_bumper){
-            motorArm.setPower(1);
-        }
-        if (gamepad1.left_bumper) {
-            motorArm.setPower(-1);
-        }
-        float x = gamepad1.left_stick_y;
-        float y = gamepad1.right_stick_y;
-        if(gamepad1.left_stick_y>5)
-            motorLWheel.setPower(x);
-        if(gamepad1.right_stick_y>5)
-            motorRWheel.setPower(y);
+        motorLFWheel.setPower(0);
+        motorLBWheel.setPower(0);
+        motorRFWheel.setPower(0);
+        motorRBWheel.setPower(0);
+        // STICK VALUES
+        float leftStickX = gamepad1.left_stick_x;
+        float leftStickY = gamepad1.left_stick_y;
+        float rightStickX = gamepad1.right_stick_x;
+        float rightStickY = gamepad1.right_stick_y ;
+        // MOVEMENT
+        motorLFWheel.setPower(leftStickY);
+        motorLBWheel.setPower(leftStickY);
+        motorRFWheel.setPower(leftStickY);
+        motorRBWheel.setPower(leftStickY);
+        // ARM CONTROL
+        if (gamepad1.right_bumper){motorArm.setPower(1);}
+        if (gamepad1.left_bumper) {motorArm.setPower(-1);}
     }
 }
